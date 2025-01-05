0ea77d74f0e7c7dfb50da6457183a91b0efda51f
Projacktor <1234xl.com@gmail.com>
2025-01-05 19:18:01 +0300

    chore: finish partnership.vue

--- frontend/pages/partnership.vue
+++ frontend/pages/partnership.vue
@@ -11,6 +11,18 @@
     <HomeBlock9 />
     <PartenrshipBlock4 />
     <PartenrshipBlock5 />
+    <HomeBlock5 />
+    <ApartmentRenovationBlock3 />
+    <HomeBlock6 />
+    <HomeBlock7 />
+    <HomeBlock8 />
+    <HomeBlock10 />
+    <ApartmentRenovationBlock45 />
+    <HomeBlock12 />
+    <HomeBlock13 />
+    <HomeBlock14 />
+    <HomeBlock15 />
+    <HomeBlock16 />
   </NuxtLayout>
 </template>
 
