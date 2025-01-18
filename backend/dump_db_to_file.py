da362a2f3e3def10b67ef906abe19372bcff98bc
lanebo1 <k.efimovich@innopolis.university>
2025-01-18 12:28:51 +0300

    db dump

--- backend/dump_db_to_file.py
+++ backend/dump_db_to_file.py
@@ -7,7 +7,8 @@ load_dotenv()
 
 def dump_db_to_file(db_name, db_user, db_password, db_host, db_port, file_path):
     os.environ['PGPASSWORD'] = db_password
-    command = f'pg_dump -U {db_user} -h {db_host} -p {db_port} {db_name} > {file_path}'
+    pg_dump_path = 'C:/Program Files/PostgreSQL/17/bin/pg_dump.exe'
+    command = f'"{pg_dump_path}" -U {db_user} -h {db_host} -p {db_port} {db_name} > {file_path}'
     subprocess.run(command, shell=True)
 
 
--- readme.md
+++ readme.md
@@ -1,7 +1,1 @@
-# on clear VM
-
-- make prepare_ansible
-- make create_remote_user
-- make set_environment
-
-- make deploy_frontend
+# FixRemont
