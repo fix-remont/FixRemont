0f81bb2c604025d317256ded4d593a884afba437
Projacktor <1234xl.com@gmail.com>
2025-03-03 23:30:21 +0300

    chore: change subtitle-glob and refactor elastic layout for apartment-renovation/block_4_5

--- frontend/components/apartment-renovation/block_4_5.vue
+++ frontend/components/apartment-renovation/block_4_5.vue
@@ -8,120 +8,53 @@ const items = [
 </script>
 
 <template>
-  <div class="block2 margin-glob">
-    <h1 class="title title-glob">
-      Лично <span class="orange"> несу ответственность </span> за качество предоставляемых услуг
-    </h1>
-    <div class="img-container">
-      <img class="img" src="/images/home/block_11.png" alt="img" />
-      <div class="text-container">
-        <p class="text1">Фанис Хасанов</p>
-        <p class="text2">
+  <div class="margin-glob-new grid grid-cols-1 gap-[2vw] md:grid-cols-2">
+    <div class="md:flex md:flex-col md:justify-between">
+      <section>
+        <h1 class="text-glob-xl mb-[2vw]">
+          Лично
+          <span class="text-primary">
+            несу <br />
+            ответственность <br />
+          </span>
+          за качество <br />
+          предоставляемых услуг
+        </h1>
+        <p class="subtitle-glob mb-[2em] md:w-4/5">
+          За 10 лет мы выполнили более 1 000 объектов и не получили ни одной жалобы на качество
+          работы и обслуживания
+        </p>
+      </section>
+      <div class="mb-[4vw] grid grid-cols-1 gap-[4vw] md:mb-0 md:grid-cols-2 md:gap-[1vw]">
+        <div
+          class="flex flex-col gap-[1vw] rounded-[4vw] bg-white p-[6vw] md:h-[10vw] md:flex-row md:justify-between md:rounded-[1vw] md:p-[2.2vw]"
+          v-for="(item, index) in items"
+        >
+          <span
+            class="orange order-2 mt-[.3vw] h-[4vw] w-[4vw] self-end font-bold md:h-[1vw] md:w-[1vw] md:self-start"
+          >
+            0{{ index + 1 }}
+          </span>
+          <span class="order-1 me-[10vw] text-[4vw] md:me-0 md:flex md:items-end md:text-[.9vw]">
+            {{ item }}</span
+          >
+        </div>
+      </div>
+    </div>
+    <div class="rounded-glob-new relative">
+      <img class="h-full w-full object-cover" src="/images/home/block_11.png" alt="img" />
+      <div class="absolute bottom-[6vw] left-[6vw] text-white md:bottom-[3vw] md:left-[3vw]">
+        <p
+          class="mb-[4vw] text-[6vw] font-semibold leading-[8vw] md:mb-[2vw] md:text-[2vw] md:leading-[2.5vw]"
+        >
+          Фанис <br />
+          Хасанов
+        </p>
+        <p class="text-[3vw] md:text-[.8vw]">
           Генеральный директор <br />
           FIX РЕМОНТ
         </p>
       </div>
     </div>
-    <div class="items">
-      <div class="item" v-for="item in items" :key="item">
-        <img class="icon" src="/icons/checkbox__checked__orange.svg" alt="icon" />
-        <span class="text"> {{ item }}</span>
-      </div>
-    </div>
   </div>
 </template>
-
-<style scoped>
-.block2 {
-  display: grid;
-  grid-template-areas: 'title img' 'items img';
-  grid-template-columns: 700px auto;
-  gap: 30px;
-
-  @media (max-width: 1240px) {
-    grid-template-areas: 'title' 'img' 'items';
-    grid-template-columns: auto;
-    gap: 10px;
-  }
-
-  .title {
-    grid-area: title;
-  }
-
-  .img-container {
-    grid-area: img;
-    display: flex;
-    position: relative;
-    color: white;
-
-    .img {
-      width: 100%;
-      height: auto;
-      object-fit: cover;
-      border-radius: 35px;
-
-      /* object-position: ; */
-      @media (max-width: 800px) {
-        border-radius: 20px;
-      }
-    }
-
-    .text-container {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-      bottom: 20px;
-      left: 20px;
-      position: absolute;
-
-      .text1 {
-        font-size: 32px;
-        font-weight: 600;
-
-        @media (max-width: 800px) {
-          font-size: 20px;
-        }
-      }
-
-      .text2 {
-        font-size: 15px;
-        font-weight: 500;
-
-        @media (max-width: 800px) {
-          font-size: 12px;
-        }
-      }
-    }
-  }
-
-  .items {
-    grid-area: items;
-    display: flex;
-    flex-wrap: wrap;
-    row-gap: 30px;
-    justify-content: space-between;
-
-    .item {
-      display: flex;
-      gap: 10px;
-      font-size: 18px;
-      font-weight: 500;
-      align-items: flex-start;
-      width: 350px;
-
-      @media (max-width: 800px) {
-        width: auto;
-        font-size: 16px;
-      }
-
-      .icon {
-        position: relative;
-        top: 4px;
-      }
-
-      .text {
-      }
-    }
-  }
-}
-</style>
