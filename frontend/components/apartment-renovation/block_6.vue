851b15b98ed3def0bafb8072162799d8c42511bc
Projacktor <1234xl.com@gmail.com>
2025-03-04 02:37:18 +0300

    chore: update elastic layout for apartments-renovation/block_6

--- frontend/components/apartment-renovation/block_6.vue
+++ frontend/components/apartment-renovation/block_6.vue
@@ -3,81 +3,29 @@ import { hrefCalculater } from 'assets/variables'
 </script>
 
 <template>
-  <div class="margin-glob block">
-    <div class="no-risk-block">
-      <div class="text-container">
-        <h4>Исключите риски и доверьте свой объект FIX-ремонт</h4>
-        <h5>мы не просто берём все финансовые риски на себя, но и закрепляем их юридически</h5>
+  <div class="margin-glob flex flex-col gap-8">
+    <div
+      class="flex flex-col items-center justify-around gap-8 rounded-[24px] bg-[var(--c-orange)] p-[3vw] md:flex-row md:p-[2vw]"
+    >
+      <div class="flex flex-col gap-2 font-semibold text-white">
+        <h4 class="text-center text-[4.5vw] md:text-left md:text-[2vw]">
+          Исключите риски и доверьте свой объект FIX-ремонт
+        </h4>
+        <h5 class="text-center text-[3vw] md:text-left md:text-[1.2vw]">
+          мы не просто берём все финансовые риски на себя, но и закрепляем их юридически
+        </h5>
       </div>
-      <NuxtLink :to="hrefCalculater">
-        <SharedButton> Онлайн-калькулятор </SharedButton>
-      </NuxtLink>
+      <UButton class="p-[3vw] md:w-[20vw] md:p-[1.5vw]" color="white" :to="hrefCalculater"
+        >Онлайн-калькулятор</UButton
+      >
     </div>
   </div>
 </template>
 
 <style scoped>
-.block {
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-}
-
-.no-risk-block {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 35px;
-  border-radius: 35px;
-  justify-content: space-around;
-  background-color: var(--c-orange);
-
-  button {
-    background-color: var(--c-black);
-    color: #fff;
-    border: transparent;
-  }
-}
-
-.text-container {
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  color: #fff;
-
-  h4 {
-    font-weight: bolder;
-    font-size: 40px;
-  }
-
-  h5 {
-    font-weight: 450;
-    font-size: 25px;
-  }
-}
-
-@media (max-width: 800px) {
-  .no-risk-block {
-    flex-direction: column;
-    align-items: center;
-    gap: 2rem;
-    padding: 20px;
-
-    a {
-      width: 100%;
-    }
-  }
-
-  .text-container {
-    align-items: center;
-    text-align: center;
-
-    h4 {
-      font-size: 28px;
-    }
-    h5 {
-      font-size: 16px;
-    }
+@media (max-width: 768px) {
+  a {
+    width: 100%;
   }
 }
 </style>
